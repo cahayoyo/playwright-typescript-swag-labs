9f3044911fde0e8e3cc09bf5a996199654696636
cahayoyo <cahayoyo@gmail.com>
2026-05-12 16:34:13 +0700

    headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
